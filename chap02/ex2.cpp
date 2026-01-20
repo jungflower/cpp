1b672a270d6f5784bd5b760db894165b53d74354
junghwalee <junghwalee@junghwaleemac.local>
2026-01-20 17:46:02 +0900

    Add annotation

--- chap02/ex2.cpp
+++ chap02/ex2.cpp
@@ -18,7 +18,7 @@ int main(){
 
     Point *ptr1 = new Point{1, 2};
     Point *ptr2 = new Point{3, 4};
-    Point& res = PntAdder(*ptr1, *ptr2);
+    Point& res = PntAdder(*ptr1, *ptr2); // 참조도 받아서 나중에 메모리 해제 고려
     cout << res.xpos << " " << res.ypos << endl;
 
     delete ptr1;
